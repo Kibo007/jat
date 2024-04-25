b4655aca7a4f6cf479d664d1c35037d80272df1e
Bojan Dragojevic <bdragojevic@gmail.com>
2024-04-25 19:15:33 +0200

    Stop propagation click on checkbox.

--- components/PositionsTable/Columns.tsx
+++ components/PositionsTable/Columns.tsx
@@ -39,6 +39,9 @@ export const Columns: ColumnDef<Position>[] = [
         checked={row.getIsSelected()}
         onCheckedChange={(value) => row.toggleSelected(!!value)}
         aria-label="Select row"
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
       />
     ),
     enableSorting: false,
